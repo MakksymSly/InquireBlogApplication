c7982a016c492fa4cdaf4394824361a234bf4058
Maksym Seliutin <maxwell7k@gmail.com>
2025-07-08 14:34:55 +0300

    fix: configure database connection and resolve TypeORM dependency injection. feat: add env.example

--- blog-backend/src/app.module.ts
+++ blog-backend/src/app.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
+import { Post } from './posts/entities/post.entity';
+import { Comment } from './comments/entities/comment.entity';
 
 @Module({
-  imports: [PostsModule, CommentsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST!,
+      port: parseInt(process.env.DB_PORT!),
+      username: process.env.DB_USERNAME!,
+      password: process.env.DB_PASSWORD!,
+      database: process.env.DB_NAME!,
+      entities: [Post, Comment],
+      synchronize: process.env.NODE_ENV !== 'production',
+    }),
+    PostsModule,
+    CommentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
